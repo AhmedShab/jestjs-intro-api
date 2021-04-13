84caf5522d76b31e8bcb3918ab8b46431326b80f
AhmedShab <hello@ahmedshaaban.tech>
2021-04-14 06:44:54 +1200

    added guards, interceptor and basic test setup for the coffee service

--- src/coffees/coffees.service.spec.ts
+++ src/coffees/coffees.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getConnectionToken, getModelToken } from '@nestjs/mongoose';
 import { CoffeesService } from './coffees.service';
+import { Coffee } from './entities/coffee.entity';
+import { Event } from '../events/entities/event.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
 
 describe('CoffeesService', () => {
   let service: CoffeesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CoffeesService],
+      providers: [
+        CoffeesService,
+        { provide: getConnectionToken(), useValue: {} },
+        { provide: getModelToken(Coffee.name), useValue: {} },
+        { provide: getModelToken(Event.name), useValue: {} },
+        { provide: COFFEE_BRANDS, useValue: {} },
+      ],
     }).compile();
 
     service = module.get<CoffeesService>(CoffeesService);
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ApiKeyGuard } from './common/guards/api-key.guard';
+import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +16,8 @@ async function bootstrap() {
       },
     }),
   );
+  app.useGlobalGuards(new ApiKeyGuard());
+  app.useGlobalInterceptors(new WrapResponseInterceptor());
   await app.listen(3000);
 }
 bootstrap();
