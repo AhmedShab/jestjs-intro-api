73654628c3e264f1887bb49e1e9227c6f374178d
AhmedShab <hello@ahmedshaaban.tech>
2021-04-15 16:27:57 +1200

    create config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,8 +7,11 @@ import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     CoffeesModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-course'),
+    MongooseModule.forRoot(
+      `mongodb://${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`,
+    ),
   ],
   controllers: [AppController],
   providers: [AppService],
