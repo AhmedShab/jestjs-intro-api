7dc545d59141259dfbfd24bc62be533afdb16d93
AhmedShab <hello@ahmedshaaban.tech>
2021-04-15 16:43:02 +1200

    added validation for the .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(27017),
+      }),
+    }),
     CoffeesModule,
     MongooseModule.forRoot(
       `mongodb://${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`,
